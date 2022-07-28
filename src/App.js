976f944fef6e119507da74c6988f1d9ddd8c831f
Michael Salerno <msalerno1993@gmail.com>
2022-07-28 16:26:49 -0400

    Redid CSS of application

--- src/App.js
+++ src/App.js
@@ -3,28 +3,56 @@ import {useState} from 'react'
 
 function App() {
 
-  const [text, setText] = useState('')
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
+  const [email, setEmail] = useState('')
+  const [gender, setGender] = useState('Male')
 
-  const clickHandle = (e) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    setText(text)
-    setText('')
+    setFirst(first)
+    
   }
-
   return (
-    <h1 className="App">
-        Form Application
+    <div className="create">
+        <h1>Form Application</h1>
+        <form onSubmit={handleSubmit}>
+          <label>First Name</label>
+          <input
+            type='text'
+            required
+            value={first}
+            onChange={(e) => setFirst(e.target.value)}
+          />
+          <label>Last Name</label>
+          <input
+            type='text'
+            required
+            value={last}
+            onChange={(e) => setLast(e.target.value)}
+          />
+          <label>Email Address</label>
+          <input
+            type='text'
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <select
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
+            <option value='Choose Not To Say'>Choose Not To Say</option>
+          </select>
+          <button>Submit Information</button>
+        </form>
         <div>
-          <form onChange={(e) => setText(e.target.value)} value={text} placeholder='Type Here'>
-            <input>
-            </input>
-          </form><br></br>
-          <button onSubmit={clickHandle}>Submit</button>
+          {first}
         </div>
-        <p>
-          {text}
-        </p>
-    </h1>
+    </div>
   );
 }
 
